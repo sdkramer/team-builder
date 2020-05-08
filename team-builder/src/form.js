8966bbff82cf97bca2fcc901d86f4c451c76afbc
Sean Kramer <seandominickramer@gmail.com>
2020-05-08 16:56:34 -0400

    Add fieldset element to form

--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,41 +1,46 @@
-import React from 'react'
+import React from "react";
 
-function MembersForm(props){
-  const {
-    onInputChange,
-    onSubmit,
-    values
-  } = props
+function MembersForm(props) {
+  const { onInputChange, onSubmit, values } = props;
 
   return (
     <form>
-      <h2>Members Form</h2>
-      <label>Username:&nbsp;
-        <input
-        value={values.username}
-        onChange={onInputChange}
-        name='username'
-        type='text'/>
-      </label>
-      <br></br>
-      <label>Email:&nbsp;
-        <input
-        value={values.email}
-        onChange={onInputChange}
-        name='email'
-        type='text'/>
-      </label>
-      <br></br>
-      <label>Role:&nbsp;<input
-      value={values.role}
-      onChange={onInputChange}
-      name='role'
-      type='text'/></label>
-      <br></br>
-      <button onSubmit={onSubmit}>Submit</button>
+      <fieldset>
+        <legend>Members Form</legend>
+        <label>
+          Username:&nbsp;
+          <input
+            value={values.username}
+            onChange={onInputChange}
+            name="username"
+            type="text"
+          />
+        </label>
+        <br></br>
+        <label>
+          Email:&nbsp;
+          <input
+            value={values.email}
+            onChange={onInputChange}
+            name="email"
+            type="text"
+          />
+        </label>
+        <br></br>
+        <label>
+          Role:&nbsp;
+          <input
+            value={values.role}
+            onChange={onInputChange}
+            name="role"
+            type="text"
+          />
+        </label>
+        <br></br>
+        <button onSubmit={onSubmit}>Submit</button>
+      </fieldset>
     </form>
-    
-  )
+  );
 }
 
-export default MembersForm
+export default MembersForm;
